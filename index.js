4427ba692803a91c588247b705a9192ea295ff97
eric.massarioli <eric.massarioli@gmail.com>
2024-09-12 13:45:51 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const { select, input } = require('@inquirer/prompts')
 
+let meta = {
+    value: "Tomar 3L de água por dia",
+    checked: false
+}
+
+let metas = [meta]
+
 const cadastrarMeta = async () => {
-    const meta = await input({message: "Digite a meta:"})
+    const meta = await input({ message: "Digite a meta:"})
 
     if(meta.length == 0) {
         console.log("A meta não pode ser vazia")
         return
     }
-}
 
+    metas.push(
+        {value: meta, checked: false}
+    )    
+}
 // sempre que usamos o await na função temos que usar o async
 // assincrona é porque as informações podem aguardar uma resposta por exemplo 
 const start = async() => {
@@ -38,6 +48,7 @@ const start = async() => {
         switch(option) {
             case "cadastrar":
                 await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("vamos listar")
