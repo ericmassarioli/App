9c17a40fe60a03d17263e7d549084fef3c51a72e
eric.massarioli <eric.massarioli@gmail.com>
2024-09-13 11:06:58 -0300

    RESOLVE PROBLEMA MARCADAS E DESMARCADAS

--- index.js
+++ index.js
@@ -26,15 +26,16 @@ const listarMetas = async () => {
         choices: [...metas]
     })
 
-    /*if (respostas.length == 0) {
+    metas.forEach((m)=>{
+        m.checked = false
+    })
+
+    if (respostas.length == 0) {
         console.log("Nenhuma meta selecionada")
         return
-    }*/
+    }
 
-    metas.forEach((m)=>{
-         m.checked = false
-    })
-    // forEach é para cada um
+      // forEach é para cada um
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
